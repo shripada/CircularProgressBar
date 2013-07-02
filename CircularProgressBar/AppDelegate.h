025feac127429c166e13b297f918edccdf9be604
shripada <shripadah@gmail.com>
2013-07-02 16:42:22 +0530

    Initial Commit

---
+++ CircularProgressBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircularProgressBar
+//
+//  Created by nsh on 02/07/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CircularProgressBar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CircularProgressBar
+//
+//  Created by nsh on 02/07/13.
+//  Copyright (c) 2013 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
